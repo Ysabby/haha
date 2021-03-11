9274c1b7505ccda61585b1a2c9c0954ed7a6ade5
yinshuai <1759628592@qq.com>
2021-03-11 16:08:02 +0800

    Initial commit

---
+++ 111.py
@@ -0,0 +1,55 @@
+#tinydict={'name':'abc','age':25,'sex':'woman'}
+#print (tinydict)
+
+#list=['abc',52,2.25,'jadh',36854]
+#print(list[1:3])
+
+
+"""强制数值转换
+a=154
+#print(type(a))
+
+float(a)
+print(type(a))"""
+
+
+
+#a=21
+#b=10
+#c=0
+#c=a+b
+#if c<0:
+#  print("hahah")
+#elif c>100:
+#print("djdfksdf100")
+#else:
+#  print("111")"""
+
+#a = 1
+#while a<10:
+# a += 1
+# if a%2 >0:
+#     continue
+# print(a)
+
+#i=1
+#while 1:
+#    print(i)
+#    i += 1
+#     if i > 10:
+#        break
+
+fruits = ['haha','apple','organ','haj']
+print(range(len(fruits)))
+for abc in range(len(fruits)):
+    print(abc)
+    print("当前水果是：",fruits[abc])
+    a = 1
+
+    print(type(a))
+
+    pr
+
+
+
+
---
+++ 123.py
@@ -0,0 +1,5 @@
+import math
+
+aaa =(125,'jadh',777,'sdhjf')
+list1=list(aaa)
+print(list1)
